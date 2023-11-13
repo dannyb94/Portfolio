0e32e0d00a234b74c30a24517af393db82e2d8a5
dannyb94 <dannynicole28@gmail.com>
2023-11-12 18:59:16 -0800

    11/11/23

--- mid23-portfolio/src/Components/Pages/Home.js
+++ mid23-portfolio/src/Components/Pages/Home.js
@@ -11,12 +11,85 @@
 
 import React from 'react';
 import {Link} from 'react-router-dom';
+import { LoremIpsum } from "react-lorem-ipsum";
+import PlaceHolder from '../../Assets/pexels-test-img.jpg'
 
 export default function Grid() {
     return (
-        <div id='MainCntnr'>
-            <Link className='box box1' to='/Project1'><div>{/* <img src='https://placehold.co/600x400'/> */}</div></Link>
-            <div className='box box2'>
+        <div id='FlexGridHome'>
+            <Link className='boxH1 sameBox1' to='/Project1'>
+                <div>
+                    <img src={PlaceHolder} alt='placeholder'/>
+                </div>
+            </Link>
+            <div className='boxH1 sameBox1'>
+                <p><LoremIpsum avgWordsPerSentence={12} avgSentencesPerParagraph={4}/></p>
+            </div>
+
+            <div className='break'></div>
+
+            {/* Another flex grid of 3-6 to get the columns I want. */}
+            <Link className='boxH1 sameBox2' to='/Project3'>
+                <div>
+                    <img src={PlaceHolder} alt='placeholder'/>
+                </div>
+            </Link>
+            <div className='boxH1 sameBox2'>
+                <div>
+                    <p>Social Media icon links</p>
+                    <a href='https://www.linkedin.com/in/danielle-ussery-668961a3/'><p>LinkedIn</p></a>
+                    <a href='https://github.com/dannyb94?tab=repositories'><p>GitHub</p></a>
+                    <a href='https://www.behance.net/dannynicol4882'><p>Behance - maybe</p></a>
+                </div>
+            </div>
+            <Link className='boxH1 soloBox1' to='/Project5'>
+                <div>
+                    <img src={PlaceHolder} alt='placeholder'/>
+                </div>
+            </Link>
+            <Link className='boxH1 sameBox3' to='/Project6'>
+                <div>
+                    <img src={PlaceHolder} alt='placeholder'/>
+                </div>
+            </Link>
+
+            <div className='break'></div>
+
+            <Link className='boxH1 soloBox2' to='/Project7'>
+                <div>
+                    <img src={PlaceHolder} alt='placeholder'/>
+                </div>
+            </Link>
+            
+            <Link className='boxH1 sameBox2' to='/Project8'>
+                <div>
+                    <img src={PlaceHolder} alt='placeholder'/>
+                </div>
+            </Link>
+            <Link className='boxH1 sameBox3' to='/Project9'>
+                <div>
+                    <img src={PlaceHolder} alt='placeholder'/>
+                </div>
+            </Link>
+            <Link className='boxH1 sameBox2' to='/Project10'>
+                <div>
+                    <img src={PlaceHolder} alt='placeholder'/>
+                </div>
+            </Link>
+
+
+
+
+
+
+
+
+
+
+
+
+            {/* <Link className='box sameBox1' to='/Project1'><div><img src='https://placehold.co/600x400'/></div></Link>
+            <div className='box sameBox1'>
                 <div className='boxInner'>
                     <p>Say "Hey"</p>
                     <p>What you do</p>
@@ -32,11 +105,11 @@ export default function Grid() {
                 </div>
             </div>
             <Link className='box box5' to='/Project5'><div></div></Link>
-            <Link className='box box6' to='/Project6'><div></div></Link>
+            <Link className='box sameBox1' to='/Project6'><div></div></Link>
             <Link className='box box7' to='/Project7'><div></div></Link>
             <Link className='box box8' to='/Project8'><div></div></Link>
-            <Link className='box box9' to='/Project9'><div></div></Link>
-            <Link className='box box10' to='/Project10'><div></div></Link>
+            <Link className='box sameBox1' to='/Project9'><div></div></Link>
+            <Link className='box box10' to='/Project10'><div></div></Link> */}
         </div>
     );
 };
--- mid23-portfolio/src/Components/Pages/Project10.js
+++ mid23-portfolio/src/Components/Pages/Project10.js
@@ -4,16 +4,28 @@ import { LoremIpsum } from "react-lorem-ipsum";
 export default function Project10() {
     return (
         <div>
-            <div className='FlexGridTop'>
+            <p>Hello</p>
+
+            <p>
+                <span>E-Mail</span>
+                <span>//</span>
+                <span>emailaddress@email.com</span>
+            </p>
+
+
+
+
+
+            {/* <div className='FlexGridTop'>
                 <div className='col col1'>
-                    {/* <img src='https://placehold.co/1000x350' alt='placeholder'/> */}
                 </div>
             </div>
             <div className='FlexGridTop'>
                 <div className='col col1-2'>
+                    <p>Hello</p>
                     <p><LoremIpsum avgWordsPerSentence={12} avgSentencesPerParagraph={4}/></p>
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 };
